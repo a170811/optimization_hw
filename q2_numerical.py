08f7a2668ab889a1ddf78228d07cc40c4a236dc9
a170811 <a170811@gmail.com>
2019-05-22 00:10:44 +0800

    q2 half-done (order under 3)

--- q2_numerical.py
+++ q2_numerical.py
@@ -1,6 +1,9 @@
 import numpy as np
-from golden_search import golden_search
 import sys
+import time
+
+from golden_search import golden_search
+from armijo import armijo
 
 
 class quadratic():
@@ -28,7 +31,7 @@ class quadratic():
         res = self.val(x, coefs)
         return np.sum((y - res)**2)
 
-    def fletcher_reeves(self, x, y, n = 10, epsilon = 0.0005):
+    def fletcher_reeves(self, x, y, epsilon = 0.0005):
 
         x = np.array(x, dtype = np.float64)
         y = np.array(y, dtype = np.float64)
@@ -40,14 +43,16 @@ class quadratic():
 
         counter = 0
         while np.sqrt(np.sum(s**2)) > epsilon:
+
             f = lambda lamda: loss(self.coefs + lamda * s)
-            res = golden_search(f, [-1000000, 1000000])
+            ran = armijo(f)
+            res = golden_search(f, [0, ran*1.2])
             self.coefs += s * res
 
             g_next = gradient(loss, self.coefs)
             s = -g_next + g_next.dot(g_next)/g.dot(g) * s
             counter += 1
-            if counter < n:
+            if counter < self.order:
                 g = g_next
             else:
                 g = gradient(loss, self.coefs)
@@ -70,25 +75,34 @@ def gradient(func, coefs):
 
 def q1_result():
 
-    print('Part1-1: Linear regression with fletcher_reeves')
+    t1 = time.time()
+    print('Part1-1: Linear regression with fletcher_reeves\n')
     func = quadratic(2)
-    func.fletcher_reeves(x, y, 10)
+    func.fletcher_reeves(x, y)
+    t2 = time.time()
 
     print('\tThe coefficients a0 and a1 for function (a0 + a1 * x) is :\n\ta0 = {:.3f}\n\ta1 = {:.3f}'.format(func.coefs[0], func.coefs[1]))
+    print('\n\tIt cost {:.3f} seconds.'.format(t2 - t1))
     print('\n\n')
+
+
 
 def q2_result():
 
+    t1 = time.time()
     order = 3
-    print('Part1-2: Quadratic regression with fletcher_reeves')
+    print('Part1-2: Quadratic regression with fletcher_reeves\n')
     func = quadratic(order)
-    func.fletcher_reeves(x, y, 10)
+    func.fletcher_reeves(x, y)
 
+    t2 = time.time()
     assert order == len(func.coefs)
     print('\tFor order k = {}'.format(order))
     print('\tcoefficients are as below:\n')
     for i, c in enumerate(func.coefs):
         print('\ta{} = {:.5f}'.format(i, c))
+
+    print('\n\tIt cost {:.3f} seconds.'.format(t2 - t1))
 
 if '__main__' == __name__:
 
