6980a746c63573ef9e43984d4a1fbcd5241b20c5
a170811 <a170811@gmail.com>
2019-06-19 09:22:47 +0800

    fix timer

--- hw5/timer.py
+++ hw5/timer.py
@@ -1,1 +1,9 @@
-/home/a170811/others/my_tools/timer.py
+import time
+
+class timer():
+    def __init__(self, log):
+        self.log = log
+    def __enter__(self):
+        self.s = time.time()
+    def __exit__(self, *argv):
+        print(self.log, time.time() - self.s, ' sec')
